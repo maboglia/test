8a5dac6edc557bf7d5bd1a22bdea3899bbe7bd8b
maboglia <mauro.bogliaccino@gmail.com>
2014-09-02 16:51:19 +0200

    primo caricamento

---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+/home/mauro/ScegliNome/platforms/android/ant-build/classes.dex : \
+/home/mauro/ScegliNome/platforms/android/ant-build/classes/it/maboglia/sceglinome/R$xml.class \
+/home/mauro/ScegliNome/platforms/android/ant-build/classes/it/maboglia/sceglinome/BuildConfig.class \
+/home/mauro/ScegliNome/platforms/android/ant-build/classes/it/maboglia/sceglinome/ScegliNome.class \
+/home/mauro/ScegliNome/platforms/android/ant-build/classes/it/maboglia/sceglinome/R.class \
+/home/mauro/ScegliNome/platforms/android/ant-build/classes/it/maboglia/sceglinome/R$attr.class \
+/home/mauro/ScegliNome/platforms/android/ant-build/classes/it/maboglia/sceglinome/R$string.class \
+/home/mauro/ScegliNome/platforms/android/ant-build/classes/it/maboglia/sceglinome/R$drawable.class \
+/home/mauro/ScegliNome/platforms/android/ant-build/dexedLibs/classes-abb8b89fa52cd288cbc86abd3f829a02.jar \
